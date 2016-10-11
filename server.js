41c880426507d6970ee03232a10d5ecc73ef0e2e
apvenkatesan <apvenkatesan@users.noreply.github.com>
2016-10-11 17:25:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,6 +93,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req,res) {
+    articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName])); 
 });
 
